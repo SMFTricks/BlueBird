830f04591c6f11106a3b3ee03275ab49ff7710e1
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-06 17:15:16 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0 RC5; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'BluedBird.<br /><br />Author: <a href="https://smftricks.com">SMF Tricks</a>';
+
+?>
---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,45 @@
+<?php
+$txt['sh_copy_own'] = 'Enter your own copyrights';
+$txt['sh_copy_own_desc'] = 'HTML allowed';
+
+$txt['sh_desc_own'] = 'Add your own description';
+$txt['sh_desc_own_desc'] = 'Here you can add your own description in the index of the forum';
+$txt['sh_desc_own_default'] = 'Add your own description in your admin center';
+
+$txt['sh_sticky_topic'] = 'Sticky Topics';
+$txt['sh_normal_topic'] = 'Normal Topics';
+
+$txt['sh_cbox'] = 'Custom Box';
+$txt['sh_cbox_enable'] = 'Enable Custom Box';
+$txt['sh_cbox_desc'] = 'Whit this, you have possibility to put a custom box in your info center.';
+$txt['sh_cbox_t'] = 'Custom Box Title';
+$txt['sh_cbox_cont'] = 'Custom Box Content';
+$txt['sh_cbox_cont_desc'] = 'HTML allowed';
+$txt['sh_cbox_cont_default'] = 'You can add your own content in Custom Box settings';
+
+$txt['separate_sticky_locked'] = 'Separate sticky topics';
+$txt['separate_sticky_locked_desc'] = 'With this, you can separate the sticky topics and your normal topics';
+
+$txt['sh_any_com'] = 'No comments for this topic.';
+
+$txt['link_top'] = 'The title of your link';
+$txt['link_top_desc'] = 'This links appears above your forum, in the top bar.';
+$txt['link_top_url'] = 'The url of your title';
+$txt['link_top1'] = 'Link 1';
+$txt['link_top2'] = 'Link 2';
+$txt['link_top3'] = 'Link 3';
+$txt['link_top4'] = 'Link 4';
+$txt['link_top5'] = 'Link 5';
+
+$txt['sh_status'] = 'Status';
+$txt['searchbox_enable'] = 'Enable Search Box';
+$txt['searchbox_enable_desc'] = 'This will enable a search box on the menu bar of your forum.';
+
+$txt['sh_list_of_topics'] = 'List of topics of';
+$txt['sh_welcome_to'] = 'Welcome to';
+$txt['sh_list_topics'] = 'Topics list for the board';
+$txt['sh_disc_topic'] = 'This is a discussion for the topic';
+$txt['sh_the_board'] = 'the board';
+$txt['sh_adv_search'] = 'Advanced Search';
+
+?>
